6ba5591cfc6c976abfd07956ae30fe30ee4c795c
fengtp <a2209454855@gmail.com>
2017-10-11 15:32:21 +0800

    新增页面解析类示例

--- src/test/java/com/video/scanning/ApplicationTests.java
+++ src/test/java/com/video/scanning/ApplicationTests.java
@@ -1,9 +1,15 @@
 package com.video.scanning;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +19,35 @@ public class ApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public  void pull() throws IOException {
+		//获取编辑推荐页
+		Document document= Jsoup.connect("https://www.zhihu.com/explore/recommendations")
+				//模拟火狐浏览器
+				.userAgent("Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)")
+				.get();
+		Element main=document.getElementById("zh-recommend-list-full");
+		Elements url=main.select("div").select("div:nth-child(2)")
+				.select("h2").select("a[class=question_link]");
+		for(Element question:url){
+			//输出href后的值，即主页上每个关注问题的链接
+			String URL=question.attr("abs:href");
+			//下载问题链接指向的页面
+			Document document2=Jsoup.connect(URL)
+					.userAgent("Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)")
+					.get();
+			//问题
+			Elements title=document2.select("#zh-question-title").select("h2").select("a");
+			//问题描述
+			Elements detail=document2.select("#zh-question-detail");
+			//回答
+			Elements answer=document2.select("#zh-question-answer-wrap")
+					.select("div.zm-item-rich-text.expandable.js-collapse-body")
+					.select("div.zm-editable-content.clearfix");
+			System.out.println("\n"+"链接："+URL
+					+"\n"+"标题："+title.text()
+					+"\n"+"问题描述："+detail.text()
+					+"\n"+"回答："+answer.text());
+		}
+	}
 }
